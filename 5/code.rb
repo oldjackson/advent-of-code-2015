957f091069b3abd71a983ddc2ce840ed9410aafc
Lorenzo Fermi <lorenzofermi@gmail.com>
2021-04-11 22:12:04 +0200

    Add day 5 part two

--- 5/code.rb
+++ 5/code.rb
@@ -5,8 +5,13 @@ puts '*** Nice or naughty strings ***'
 puts
 
 def nice?(input_string)
+  # occurrences of any of a, e, i, o, u
   return false if input_string.scan(/[aeiou]/).size < 3
-  return false if input_string.match(/(.)\1{1}/).nil?
+  
+  # matches any character and its repetition (\1 => backreference to first matching group)
+  return false if input_string.match(/(.)\1/).nil?
+  
+  # matches ab OR cd OR /..
   return false if input_string.match(/(ab|cd|pq|xy)/)
 
   true
@@ -31,23 +36,33 @@ puts
 puts "Nice strings number: #{nice_strings_count("#{__dir__}/data.txt")}"
 puts
 
-# puts '*** Ribbon length ***'
-# puts
+puts
+puts '*** Nice or naughty strings - revised ***'
+puts
 
-# def ribbon_length(box_sizes_file_name)
-#   length = 0
+def revised_nice?(input_string)
+  # matches any group of 2 characters and its repetition with any number of any characters in the middle
+  return false if input_string.match(/(.{2}).*\1/).nil?
+  # matches any character and its repetition with any one character in the middle
+  return false if input_string.match(/(.).\1/).nil?
 
-#   File.open(box_sizes_file_name).each do |line|
-#     sorted_sizes = line.split('x').map(&:to_i).sort
+  true
+end
 
-#     length += (2 * (sorted_sizes[0] + sorted_sizes[1]) +
-#       sorted_sizes[2] * sorted_sizes[1] * sorted_sizes[0])
-#   end
+def revised_nice_strings_count(strings_file_name)
+  count = 0
 
-#   length
-# end
+  File.open(strings_file_name).each do |line|
+    count += 1 if revised_nice?(line)
+  end
 
-# puts "Expect #{ribbon_length("#{__dir__}/test_data.txt")} to eq 11369"
-# puts
-# puts "Total length: #{ribbon_length("#{__dir__}/data.txt")}"
-# puts
+  count
+end
+
+puts "Expect #{revised_nice?('qjhvhtzxzqqjkmpb')} to be true"
+puts "Expect #{revised_nice?('xxyxx')} to be true"
+puts "Expect #{revised_nice?('uurcxstgmygtbstg')} to be false"
+puts "Expect #{revised_nice?('ieodomkazucvgmuy')} to be false"
+puts
+puts "Nice strings number: #{revised_nice_strings_count("#{__dir__}/data.txt")}"
+puts
